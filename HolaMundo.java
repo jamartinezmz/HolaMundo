8d51e15b2db3b830d5eb0948f113c1c74f502b8f
jamartinezmz <jamartinezmz@gmail.com>
2017-07-03 23:46:59 -0500

    Update HolaMundo.java

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,8 +1,7 @@
-
 public class HolaMundo {
-	
-	public static void main(String[] args) {
-	    System.out.println("Hola mundo");
-	  }
-
+ 
+	public static void main(String[] args) {		
+		System.out.println("Hola Mundo");
+	}
+ 
 }
