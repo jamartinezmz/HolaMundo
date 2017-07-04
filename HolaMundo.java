f8fd18ccaaf37f88c75f8813f5d6e08705aaecae
jamartinezmz <jamartinezmz@gmail.com>
2017-07-04 00:40:30 -0500

    Update HolaMundo.java

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,6 +1,6 @@
 
 public class HolaMundo {
     public static void main(String[] args) {
-        System.out.print("Hola Mundo!");
+        System.out.print("Hola Mundo!. Este es un nuevo dia!!!");
     }
 }
